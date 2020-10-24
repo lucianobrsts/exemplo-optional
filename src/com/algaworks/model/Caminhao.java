e96ee372264d0c51cc1ceade8b6e76ab26360515
lucianobrsts <lucianobrsts@yahoo.com.br>
2020-10-24 12:06:17 -0300

    Finalizada classe seguro

--- src/com/algaworks/model/Caminhao.java
+++ src/com/algaworks/model/Caminhao.java
@@ -2,4 +2,29 @@ package com.algaworks.model;
 
 public class Caminhao {
 
+	private String modelo;
+	private Seguro seguro;
+
+	public Caminhao(String modelo, Seguro seguro) {
+		super();
+		this.modelo = modelo;
+		this.seguro = seguro;
+	}
+
+	public String getModelo() {
+		return modelo;
+	}
+
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+
+	public Seguro getSeguro() {
+		return seguro;
+	}
+
+	public void setSeguro(Seguro seguro) {
+		this.seguro = seguro;
+	}
+
 }
--- src/com/algaworks/model/Seguro.java
+++ src/com/algaworks/model/Seguro.java
@@ -1,5 +1,32 @@
 package com.algaworks.model;
 
+import java.math.BigDecimal;
+
 public class Seguro {
+
+	private String cobertura;
+	private BigDecimal valorFranquia;
+
+	public Seguro(String cobertura, BigDecimal valorFranquia) {
+		super();
+		this.cobertura = cobertura;
+		this.valorFranquia = valorFranquia;
+	}
+
+	public String getCobertura() {
+		return cobertura;
+	}
+
+	public void setCobertura(String cobertura) {
+		this.cobertura = cobertura;
+	}
+
+	public BigDecimal getValorFranquia() {
+		return valorFranquia;
+	}
+
+	public void setValorFranquia(BigDecimal valorFranquia) {
+		this.valorFranquia = valorFranquia;
+	}
 
 }
